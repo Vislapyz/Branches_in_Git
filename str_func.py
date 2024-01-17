a72e87d02e372454ab5390164cf387b1706248bf
Mihail <vislapyz@mail.ru>
2024-01-18 00:18:23 +0300

    feature/task3

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
 def func_title(value):
+    """returns a string with all capital letters"""
     return value.title()
+
+
+def new_func(value):
+    """capitalize the first letters of each word in the line"""
+    return value.upper()
